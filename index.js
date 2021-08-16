218957455a972898455b8af44938a2d46d20f1bc
David Szamek <davidszamek@gmail.com>
2021-08-16 18:49:22 +1000

    update query functions

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// === Dependencies === // 
+// Dependencies 
 
 const inquirer = require("inquirer");
 const mysql = require("mysql");
@@ -6,7 +6,7 @@ const consoleTable = require("console.table");
 const express = require("express");
 const { connect } = require("http2");
 
-// === createConnection() metho to link Node.js and MySQL === // 
+// createConnection() metho to link Node.js and MySQL 
 const db = mysql.createConnection({
 
     host: "localhost",
@@ -29,6 +29,8 @@ db.connect(function (err) {
            ╚╝      ╚══╝                     ╚══╝`)
     startTracker();
 });
+
+// startTracker app & inquirer prompt
 
 function startTracker() {
   inquirer
@@ -87,3 +89,93 @@ function startTracker() {
         }
     });
 };
+
+// Query Functions
+
+// View All Employees
+function viewAllEmployees() {
+    const query = "SELECT employee.id, employee.first_name, employe.last_name, role.title, department.department_name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id";
+    db.query(query, function (err, res) {
+        console.table(res);
+        startTracker();
+    });
+}
+
+// Add Employee 
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+               type: "input",
+               name: "firstName",
+               message: "Please enter the employee's first name.", 
+            }, 
+            {
+                type: "input",
+                name: "lastName",
+                message: "Please enter the employee's last name.", 
+             }, 
+             {
+                type: "input",
+                name: "addEmployeeRoleId",
+                message: "Please enter the employee's role ID.", 
+             }, 
+             {
+                type: "input",
+                name: "addEmployeeManagerId",
+                message: "Please enter the employee's manager ID", 
+             }, 
+        ])
+        .then(function (res) {
+            const firstName = res.firstName;
+            const lastName = res.firstName;
+            const employeeRoleId = res.addEmployeeRoleId; 
+            const employeeManagerId = res.addEmployeeManagerId;
+            const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}, "${employeeRoleId}", "${employeeManagerId}")`;
+            db.query(query, function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                console.table(res);
+                startTracker();
+            });
+        }) ;
+}
+
+// Update Employee
+function updateEmployeeRole() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "updateEmployee",
+                message: "Please enter the ID of the employee you want to be updated.", 
+             }, 
+             {
+                type: "input",
+                name: "newRole",
+                message: "Please enter the ID of the new role for that employee.", 
+             }, 
+        ])
+        .then(function (res) {
+            const updateEmployee = res.updateEmployee;
+            const newRole = res.newRole;
+            const queryUpdate = `UPDATE employee SET role_id = "${newRole}" where id = "${updateEmployee}"`;
+            db.query(queryUpdate, function (err, res) {
+                if (err) {
+                    throw err;
+                }
+                console.table(res);
+                startTracker();
+            });
+        });
+}
+
+// View All Roles 
+function viewAllRoles() {
+    const query = "SELECT * FROM role"
+    db.query(query, function (err, res){
+        console.table(res);
+        startTracker();
+    })
+}
